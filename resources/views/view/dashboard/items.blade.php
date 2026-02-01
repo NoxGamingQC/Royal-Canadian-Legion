25190868ec0bbca40fca8bac4790718e3ae817e5
Jimmy Béland-Bédard <noxgamingqc@gmail.com>
2026-01-31 23:00:09 -0500

    Remake item page

--- resources/views/view/dashboard/items.blade.php
+++ resources/views/view/dashboard/items.blade.php
@@ -5,106 +5,60 @@
     <br /><br />
     <h1>Listes des articles</h1>
     <hr />
-    <div class="row">
+    <div class="card card-body">
+        <div class="row gx-4 gy-4">
         @foreach($catalog as $category)
-            <div class="col-md-12">
-                <br /><br />
-                <h2>{{$category->fullname}}</h2>
-                <div class="row gx-4 gy-4">
                     @if(count($category->getVariations()) > 0)
                         @foreach ($category->getVariations() as $item)
                             <div class="col-md-4">
-                                <div class="card">
-                                    <div class="card-img-top" style="background-image:url({{$item->image}});height:200px;max-height:200px;overflow:hidden;background-size: cover;background-repeat: no-repeat;background-position: center;"></div>
+                                <a href="/item/{{$item->id}}" class="text-start btn btn-light card {{!$item->is_active ? 'text-bg-secondary' : (is_null ($item->inventory) ? 'border-secondary' : ($item->inventory > $item->alert_threshold ? 'border-success' : ($item->inventory == 0 ? 'border-danger text-bg-danger' : 'border-warning text-bg-warning')))}}">
                                     <div class="card-body">
-                                        <h5 class="card-title">{{$item->name}}</h5>
-                                        <div class="row gy-3">
-                                            <div class="input-group">
-                                                <span class="input-group-text">Nom: </span>
-                                                <input type="text" class="form-control" value="{{$item->name}}">
+                                        <div class="row">
+                                            <div class="col-3">
+                                                <div class="img-thumbnail" style="width:75px;height:75px;max-width:75px;max-height:75px;overflow:hidden;background-size: cover;background-repeat: no-repeat;background-position: center;background-image:url({{$item->image}});"></div>
                                             </div>
-                                            <div class="input-group">
-                                                <span class="input-group-text">Prix: </span>
-                                                <input type="text" class="form-control" value="{{$item->price}}">
-                                                <span class="input-group-text">$</span>
+                                            <div class="col-6">
+                                                <span><b>{{$item->name}}</b></span>
+                                                <br />
+                                                <small>Catégorie: {{$category->fullname}}</small>
+                                                @if($item->inventory !== null)
+                                                    <p class="badge {{is_null($item->inventory) ? 'text-bg-secondary' : ($item->inventory > $item->alert_threshold ? 'text-bg-success' : 'text-bg-danger')}}">Disponible: {{$item->inventory}}</p>
+                                                @endif
                                             </div>
-                                            <div class="input-group">
-                                                <span class="input-group-text form-control">Disponible à la vente: </span>
-                                                <div class="input-group-text">
-                                                    <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Radio button for following text input">
-                                                </div>
+                                            <div class="col-3">
+                                                ${{$item->price}}
                                             </div>
-                                            <div class="input-group">
-                                                <span class="input-group-text form-control">Calculer dans l'inventaire: </span>
-                                                <div class="input-group-text">
-                                                    <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Radio button for following text input">
-                                                </div>
-                                            </div>
-                                            <div class="input-group">
-                                                <span class="input-group-text">Alertes d'inventaire bas: </span>
-                                                <input type="text" class="form-control" value="{{$item->alert_threshold}}">
-                                            </div>
-                                            <div class="input-group mb-3">
-                                                <label class="input-group-text" for="inputGroupFile01">Image:</label>
-                                                <input type="file" class="form-control" id="inputGroupFile01">
-                                            </div>
-
-                                            <button class="btn btn-success disabled" style="margin-bottom: 10px;" disabled>Sauvegarder</button>
-                                            <hr />
-                                            <button class="btn btn-danger disabled" disabled>Supprimer</button>
                                         </div>
                                     </div>
-                                </div>
+                                </a>
                             </div>
                         @endforeach
                     @else
                         <div class="col-md-4">
-                                <div class="card">
-                                    <div class="card-img-top" style="background-image:url({{$category->image}});height:200px;max-height:200px;overflow:hidden;background-size: cover;background-repeat: no-repeat;background-position: center;"></div>
-                                    <div class="card-body">
-                                        <h5 class="card-title">Article de base</h5>
-                                        <div class="row gy-3">
-                                            <div class="input-group">
-                                                <span class="input-group-text">Nom: </span>
-                                                <input type="text" class="form-control" value="Article de base">
-                                            </div>
-                                            <div class="input-group">
-                                                <span class="input-group-text">Prix: </span>
-                                                <input type="text" class="form-control" value="{{$category->price}}">
-                                                <span class="input-group-text">$</span>
-                                            </div>
-                                            <div class="input-group">
-                                                <span class="input-group-text form-control">Disponible à la vente: </span>
-                                                <div class="input-group-text">
-                                                    <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Radio button for following text input">
-                                                </div>
-                                            </div>
-                                            <div class="input-group">
-                                                <span class="input-group-text form-control">Calculer dans l'inventaire: </span>
-                                                <div class="input-group-text">
-                                                    <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Radio button for following text input">
-                                                </div>
-                                            </div>
-                                            <div class="input-group">
-                                                <span class="input-group-text">Alertes d'inventaire bas: </span>
-                                                <input type="text" class="form-control" value="{{$category->alert_threshold}}">
-                                            </div>
-                                            <div class="input-group mb-3">
-                                                <label class="input-group-text" for="inputGroupFile01">Image:</label>
-                                                <input type="file" class="form-control" id="inputGroupFile01">
-                                            </div>
-
-                                            <button class="btn btn-success disabled" style="margin-bottom: 10px;" disabled>Sauvegarder</button>
-                                            <hr />
-                                            <button class="btn btn-danger disabled" disabled>Supprimer</button>
+                            <a href="/category/{{$category->id}}" class="text-start btn btn-light card {{!$category->is_active ? 'text-bg-secondary' : (is_null ($category->inventory) ? 'border-secondary' : ($category->inventory > $category->alert_threshold ? 'border-success' : ($category->inventory == 0 ? 'border-danger text-bg-danger' : 'border-warning text-bg-warning')))}}">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-3">
+                                            <div class="img-thumbnail" style="width:75px;height:75px;max-width:75px;max-height:75px;overflow:hidden;background-size: cover;background-repeat: no-repeat;background-position: center;background-image:url({{$category->image}});"></div>
+                                        </div>
+                                        <div class="col-6">
+                                            <span><b>Articles de base</b></span>
+                                            <br />
+                                            <small>Catégorie: {{$category->fullname}}</small>
+                                            @if($category->inventory !== null)
+                                                <p class="badge {{is_null($category->inventory) ? 'text-bg-secondary' : ($category->inventory > $category->alert_threshold ? 'text-bg-success' : 'text-bg-danger')}}">Disponible: {{$category->inventory}}</p>
+                                            @endif
+                                        </div>
+                                        <div class="col-3">
+                                            ${{$item->price}}
                                         </div>
                                     </div>
                                 </div>
-                            </div>
+                            </a>
+                        </div>
                     @endif
-                </div>
-            </div>
         @endforeach
+                </div>
     </div>
     <br /><br /><br /><br />
 </div>
